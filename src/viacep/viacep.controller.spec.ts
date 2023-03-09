129a2b67b115e1e2a95a1660048da31d94b6fe34
Rafael <rafagpoliveira@gmail.com>
2023-03-08 23:51:06 -0300

    refactoring feito e alguns testes

--- src/viacep/viacep.controller.spec.ts
+++ src/viacep/viacep.controller.spec.ts
@@ -1,13 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ViacepController } from './viacep.controller';
+import { ViacepService } from './viacep.service';
+import axios from 'axios';
 
 describe('ViacepController', () => {
   let controller: ViacepController;
 
+  const mockViacepService = {
+    getAddress: jest.fn(async cep => {
+      const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      if (data.erro) return { message: 'CEP Inválido.' };
+      return {
+        ...data
+      }
+    })
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ViacepController],
-    }).compile();
+      providers: [ViacepService]
+    }).overrideProvider(ViacepService)
+      .useValue(mockViacepService)
+      .compile();
 
     controller = module.get<ViacepController>(ViacepController);
   });
@@ -15,4 +30,27 @@ describe('ViacepController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return infos about a cep', async () => {
+    const testCep = '04884-992';
+    const errorCep = '12345-111';
+    expect(await controller.getAddress(testCep)).toEqual({
+      cep: expect.any(String),
+      logradouro: expect.any(String),
+      complemento: expect.any(String) || '',
+      bairro: expect.any(String),
+      localidade: expect.any(String),
+      uf: expect.any(String),
+      ibge: expect.any(String),
+      gia: expect.any(String) || '',
+      ddd: expect.any(String),
+      siafi: expect.any(String)
+    });
+
+    expect(await controller.getAddress(errorCep)).toEqual({
+      message: 'CEP Inválido.'
+    })
+
+    expect(mockViacepService.getAddress).toHaveBeenCalled();
+  })
 });
--- src/viacep/viacep.service.spec.ts
+++ src/viacep/viacep.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Viacep } from './entities/viacep.entity';
 import { ViacepService } from './viacep.service';
 
 describe('ViacepService', () => {
   let service: ViacepService;
+  let viacepModel: Viacep;
+
+  const mockViacepRepository = {
+    find: jest.fn().mockReset(),
+    save: jest.fn().mockImplementation(cep => Promise.resolve({
+      id: 1,
+      ...cep
+    }))
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ViacepService],
+      providers: [ViacepService, {
+        provide: getRepositoryToken(Viacep),
+        useValue: {mockViacepRepository}
+      }],
     }).compile();
 
     service = module.get<ViacepService>(ViacepService);
@@ -15,4 +29,20 @@ describe('ViacepService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return cep infos of viacep api or database', async () => {
+    const cep = '04884-992';
+    expect(await service.getAddress(cep)).toEqual({
+      cep: expect.any(String),
+      logradouro: expect.any(String),
+      complemento: expect.any(String) || '',
+      bairro: expect.any(String),
+      localidade: expect.any(String),
+      uf: expect.any(String),
+      ibge: expect.any(String),
+      gia: expect.any(String) || '',
+      ddd: expect.any(String),
+      siafi: expect.any(String)
+    })
+  })
 });
